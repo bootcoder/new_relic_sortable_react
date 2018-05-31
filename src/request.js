b09b5295af552cc6b4b8e1ddc337e1856fad23dc
BootCoder <bootcoder@gmail.com>
2018-05-31 13:26:24 -0700

    set request uri to production server

--- src/request.js
+++ src/request.js
@@ -4,7 +4,7 @@ async function fetchCustomerList (state) {
   const name = `name=${state.customerName}`
   const company = `company=${state.companyName.value}`
   const params = `customers?${sortBy}&${name}&${company}`
-  const url = `http://localhost:3000/${params}`
+  const url = `https://bootcoder-relic-search-api.herokuapp.com/${params}`
   const response = await fetch(url)
   const json = await response.json()
   await window.history.replaceState(null, null, params)
